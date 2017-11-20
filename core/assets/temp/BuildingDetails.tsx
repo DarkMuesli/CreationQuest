7b72903eb7cc5288649aad688b9422446d1e0d6a
Matthias Groß <mg-rs@gmx.de>
2017-11-20 11:15:35 +0100

    Testing tiled 1.0 files with external tilesets

---
+++ core/assets/temp/BuildingDetails.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="BuildingDetails" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="C:/Users/Matze/Pictures/TileC.png" width="512" height="512"/>
+</tileset>
---
+++ core/assets/temp/Walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Walls16" tilewidth="16" tileheight="16" tilecount="512" columns="32">
+ <image source="C:/Users/Matze/Pictures/TileA3.png" width="512" height="256"/>
+</tileset>
