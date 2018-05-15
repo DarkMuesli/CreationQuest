d125f4e2ebd6c111263157ef9607616295a31475
Matthias Groß <mg-rs@gmx.de>
2018-05-15 14:39:22 +0200

    changed to Kotlin 1.2.10, commented out Fruit.java and in Fruit.kt

--- core/src/main/java/de/thkoeln/creationquest/CreationQuestGame.java
+++ core/src/main/java/de/thkoeln/creationquest/CreationQuestGame.java
@@ -8,69 +8,69 @@ import de.thkoeln.creationquest.tiledworld.TiledWorld;
 
 public class CreationQuestGame extends Game {
 
-	private static final String TAG = CreationQuestGame.class.getName();
+    private static final String TAG = CreationQuestGame.class.getName();
 
-	private SpriteBatch spriteBatch;
-	private OrthographicCamera cam;
-	private Screen world;
-	private Screen slots;
+    private SpriteBatch spriteBatch;
+    private OrthographicCamera cam;
+    private Screen world;
+    private Screen slots;
 
-	@Override
-	public void create() {
+    @Override
+    public void create() {
 
-		Gdx.app.setLogLevel(Constants.LOG_LVL);
+        Gdx.app.setLogLevel(Constants.LOG_LVL);
 
-		Gdx.input.setInputProcessor(new InputMultiplexer(new InputAdapter() {
-			@Override
-			public boolean keyDown(int keycode) {
-				if (keycode == Input.Keys.ESCAPE) {
-					Gdx.app.log(TAG, "Spiel wurde durch ESC beendet.");
-					Gdx.app.exit();
-					return true;
-				} else
-					return false;
-			}
-		}));
+        Gdx.input.setInputProcessor(new InputMultiplexer(new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+                if (keycode == Input.Keys.ESCAPE) {
+                    Gdx.app.log(TAG, "Spiel wurde durch ESC beendet.");
+                    Gdx.app.exit();
+                    return true;
+                } else
+                    return false;
+            }
+        }));
 
-		Gdx.input.setCursorCatched(false);
+        Gdx.input.setCursorCatched(false);
 
-		EventManager.setGame(this);
+        EventManager.setGame(this);
 
-		//TODO Singleton SpriteBatch? Cam?
-		spriteBatch = new SpriteBatch();
+        //TODO Singleton SpriteBatch? Cam?
+        spriteBatch = new SpriteBatch();
 
-		// Constructs a new OrthographicCamera, using the given viewport width
-		// and height
-		// Height is multiplied by aspect ratio.
-		cam = new OrthographicCamera();
-		cam.setToOrtho(false, Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+        // Constructs a new OrthographicCamera, using the given viewport width
+        // and height
+        // Height is multiplied by aspect ratio.
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
 
-		// Load initial map
-		world = new TiledWorld("overworld.tmx", spriteBatch, cam, this);
-		slots = new SlotMachine(spriteBatch, cam, this);
-		setScreen(world);
+        // Load initial map
+        world = new TiledWorld("overworld.tmx", spriteBatch, cam, this);
+        slots = new SlotMachine(spriteBatch, cam, this);
+        setScreen(world);
 
-	}
+    }
 
-	public void setToWorld() {
-		setScreen(world);
-	}
+    public void setToWorld() {
+        setScreen(world);
+    }
 
-	public void setToSlotMachine() {
-		setScreen(slots);
-	}
+    public void setToSlotMachine() {
+        setScreen(slots);
+    }
 
-	public Screen getWorld() {
-		return world;
-	}
+    public Screen getWorld() {
+        return world;
+    }
 
-	@Override
-	public void dispose() {
-		super.dispose();
-		spriteBatch.dispose();
-		world.dispose();
-		slots.dispose();
+    @Override
+    public void dispose() {
+        super.dispose();
+        spriteBatch.dispose();
+        world.dispose();
+        slots.dispose();
 
-	}
+    }
 
 }
--- core/src/main/java/de/thkoeln/creationquest/tiledworld/Fruit.kt
+++ core/src/main/java/de/thkoeln/creationquest/tiledworld/Fruit.kt
@@ -1,47 +1,58 @@
-//package de.thkoeln.creationquest.tiledworld
-//
-//import com.badlogic.gdx.Gdx
-//import com.badlogic.gdx.audio.Sound
-//import com.badlogic.gdx.graphics.Texture
-//import com.badlogic.gdx.graphics.g2d.Sprite
-//import com.badlogic.gdx.graphics.g2d.TextureRegion
-//import com.badlogic.gdx.maps.MapObject
-//import com.badlogic.gdx.math.Vector2
-//import de.thkoeln.creationquest.utils.CoordinateHelper
-//
-//
-//class Fruit internal constructor(mapObject: MapObject, world: TiledWorld) : GameObject(mapObject, world) {
-//
-//    private var plucked: Boolean = false
-//    private var counter: Float = 0.toFloat()
-//
-//    private val text: String
-//
-//    private var pluckedSprt: Sprite? = null
-//
-//    init {
-//
-//        var row = 0
-//        var col = 0
-//        var tileSize = world.mapProp.tileHeight
-//        try {
-//            row = mapObject.properties.get("row", Int::class.java)
-//            col = mapObject.properties.get("column", Int::class.java)
-//            tileSize = mapObject.properties.get("tileSize", Int::class.java)
-//        } catch (ignored: NullPointerException) {
-//            Gdx.app.debug(TAG, "No parameters for texture detected; using defaults.")
-//            row = 0
-//            col = 0
-//            tileSize = world.mapProp.tileHeight
-//        } finally {
-//            val split = TextureRegion.split(this.sprt.texture, tileSize, tileSize)
-//            sprt = Sprite(split[row][col])
-//        }
-//
-//        val testString = mapObject.properties.get("testString", String::class.java)
-//        Gdx.app.debug(TAG, "String is: " + testString)
-//
-//
+package de.thkoeln.creationquest.tiledworld
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.maps.MapObject
+import de.thkoeln.creationquest.utils.CoordinateHelper
+
+
+class Fruit internal constructor(mapObject: MapObject, world: TiledWorld) : GameObject(mapObject, world) {
+
+    private var plucked: Boolean = false
+    private var counter: Float = 0.toFloat()
+
+    private val text: String
+
+    private var pluckedSprt: Sprite? = null
+
+    init {
+
+        var row = 0
+        var col = 0
+        var tileSize = world.mapProp.tileHeight
+        try {
+            row = mapObject.properties.get("row", Int::class.java)
+            col = mapObject.properties.get("column", Int::class.java)
+            tileSize = mapObject.properties.get("tileSize", Int::class.java)
+        } catch (ignored: NullPointerException) {
+            Gdx.app.debug(TAG, "No parameters for texture detected; using defaults.")
+            row = 0
+            col = 0
+            tileSize = world.mapProp.tileHeight
+        } finally {
+            val split = TextureRegion.split(this.sprt.texture, tileSize, tileSize)
+            sprt = Sprite(split[row][col])
+        }
+
+        val testString = mapObject.properties.get("testString", String::class.java)
+        Gdx.app.debug(TAG, "String is: " + testString)
+
+        pluckedSprt = mapObject.properties.get("pluckedPath", String::class.java)?.let {
+            val pluckedTex = Texture(it)
+            val pluckedRow = mapObject.properties.get("pluckedRow", Int::class.java) ?: 0
+            val pluckedCol = mapObject.properties.get("pluckedColumn", Int::class.java) ?: 0
+            val pluckedTileSize = mapObject.properties.get("pluckedTileSize", Int::class.java) ?: pluckedTex.height
+            val split = TextureRegion.split(pluckedTex, pluckedTileSize, pluckedTileSize)
+            Sprite(split[pluckedRow][pluckedCol])
+            //}?:sprt.also { Gdx.app.debug(TAG, "No plucked texture detected; using normal texture.") }
+            //}?:run { Gdx.app.debug(TAG, "No plucked texture detected; using normal texture."); sprt}
+        } ?: sprt.apply { Gdx.app.debug(TAG, "No plucked texture detected; using normal texture.") }
+
+
+        //Old approach
+
 //        try {
 //            val pluckedTex = Texture(mapObject.properties.get("pluckedPath", String::class.java))
 //            var pluckedRow = 0
@@ -64,64 +75,64 @@
 //            Gdx.app.debug(TAG, "No plucked texture detected; using normal texture.")
 //            pluckedSprt = sprt
 //        }
-//
-//        val v = CoordinateHelper.getPixelFromCell(x, y, world.mapProp.tileWidth.toFloat(), world.mapProp.tileHeight.toFloat())
-//        sprt.setPosition(v.x, v.y)
-//
-//        counter = 0f
-//
-//        text = words[(Math.random() * words.size).toInt()].trim { it <= ' ' }
-//    }
-//
-//    override fun onInteract(obj: GameObject): Boolean {
-//        if (!plucked && obj is Entity) {
-//            obj.pull(this)
-//        }
-//        return true
-//    }
-//
-//    override fun update(deltaTime: Float) {
-//        if (plucked) {
-//            if (counter <= FADE_TIME / 10f)
-//                sprt.translate(0f, 200f / FADE_TIME * deltaTime)
-//
-//            val newSpriteAlpha = Math.max(0f, sprt.color.a - 2 / FADE_TIME * deltaTime)
-//            sprt.setAlpha(Math.max(0f, newSpriteAlpha))
-//
-//            counter += deltaTime
-//            if ((counter) >= FADE_TIME) {
-//                removeFruit()
-//            }
-//        }
-//    }
-//
-//    fun pluckFruit() {
-//
-//        plucked = true
-//        penetrable = true
-//        counter = 0f
-//        Gdx.app.log(TAG, "Fruit is plucked.")
-//
-//        val id = sound.play()
-//        sound.setPitch(id, 1f + Math.random().toFloat() * PITCH_RANGE - PITCH_RANGE / 2)
-//
-//        world.addText(text, FADE_TIME, FADE_TIME, this)
-//
-//        sprt.setRegion(pluckedSprt!!)
-//    }
-//
-//    private fun removeFruit() {
-//        world.removeGameObject(this)
-//    }
-//
-//    companion object {
-//
-//        private val TAG = Fruit::class.java.name
-//
-//        private val FADE_TIME = 3f
-//        private val PITCH_RANGE = 0.8f
-//        private val sound = Gdx.audio.newSound(Gdx.files.internal("sounds/plop.wav"))
-//        private val words = Gdx.files.internal("text/test.txt").readString().trim { it <= ' ' }.split("\n?\r".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-//    }
-//
-//}
+
+        val v = CoordinateHelper.getPixelFromCell(x, y, world.mapProp.tileWidth.toFloat(), world.mapProp.tileHeight.toFloat())
+        sprt.setPosition(v.x, v.y)
+
+        counter = 0f
+
+        text = words[(Math.random() * words.size).toInt()].trim { it <= ' ' }
+    }
+
+    override fun onInteract(obj: GameObject): Boolean {
+        if (!plucked && obj is Entity) {
+            obj.pull(this)
+        }
+        return true
+    }
+
+    override fun update(deltaTime: Float) {
+        if (plucked) {
+            if (counter <= FADE_TIME / 10f)
+                sprt.translate(0f, 200f / FADE_TIME * deltaTime)
+
+            val newSpriteAlpha = Math.max(0f, sprt.color.a - 2 / FADE_TIME * deltaTime)
+            sprt.setAlpha(Math.max(0f, newSpriteAlpha))
+
+            counter += deltaTime
+            if ((counter) >= FADE_TIME) {
+                removeFruit()
+            }
+        }
+    }
+
+    fun pluckFruit() {
+
+        plucked = true
+        penetrable = true
+        counter = 0f
+        Gdx.app.log(TAG, "Fruit is plucked.")
+
+        val id = sound.play()
+        sound.setPitch(id, 1f + Math.random().toFloat() * PITCH_RANGE - PITCH_RANGE / 2)
+
+        world.addText(text, FADE_TIME, FADE_TIME, this)
+
+        sprt.setRegion(pluckedSprt!!)
+    }
+
+    private fun removeFruit() {
+        world.removeGameObject(this)
+    }
+
+    companion object {
+
+        private val TAG = Fruit::class.java.name
+
+        private val FADE_TIME = 3f
+        private val PITCH_RANGE = 0.8f
+        private val sound = Gdx.audio.newSound(Gdx.files.internal("sounds/plop.wav"))
+        private val words = Gdx.files.internal("text/test.txt").readString().trim { it <= ' ' }.split("\n?\r".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+    }
+
+}
